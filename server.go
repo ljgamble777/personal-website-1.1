523a9501373ad438dae72fe5b2179b68729a2cd9
Henry Fisher <henry.wfisher@gmail.com>
2020-03-23 09:00:16 -0700

    server: adding HTTPS support

--- server.go
+++ server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 	"os"
@@ -8,8 +9,14 @@ import (
 	"github.com/gorilla/handlers"
 )
 
+var crtPath = flag.String("crt", "server.crt", "certificate location")
+var keyPath = flag.String("key", "server.key", "key location")
+var staticDir = flag.String("static", "./static", "directory where static files are stored")
+
 func main() {
-	fs := http.FileServer(http.Dir("./static"))
+	flag.Parse()
+
+	fs := http.FileServer(http.Dir(*staticDir))
 	http.Handle("/", handlers.LoggingHandler(os.Stdout, fs))
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(http.ListenAndServeTLS(":4000", *crtPath, *keyPath, nil))
 }
