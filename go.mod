caf49f15a000529a1444fd617685c24f2c40456b
Henry Fisher <henry.wfisher@gmail.com>
2020-03-23 21:58:54 -0700

    server: added full https support with autocert

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module personal-website
 
 go 1.14
 
-require github.com/gorilla/handlers v1.4.2
+require (
+	github.com/gorilla/handlers v1.4.2
+	golang.org/x/crypto v0.0.0-20200323165209-0ec3e9974c59
+)
--- server.go
+++ server.go
@@ -1,23 +1,45 @@
 package main
 
 import (
+	"crypto/tls"
 	"flag"
 	"log"
 	"net/http"
 	"os"
 
 	"github.com/gorilla/handlers"
+	"golang.org/x/crypto/acme/autocert"
 )
 
-var crtPath = flag.String("crt", "server.crt", "certificate location")
-var keyPath = flag.String("key", "server.key", "key location")
 var staticDir = flag.String("static", "./static", "directory where static files are stored")
+var config = flag.String("config", "dev", "configuration to run (dev or prod)")
 
 func main() {
 	flag.Parse()
 
 	fs := http.FileServer(http.Dir(*staticDir))
 	http.Handle("/", handlers.LoggingHandler(os.Stdout, fs))
-	log.Fatal(http.ListenAndServe(":4000", nil))
-	// log.Fatal(http.ListenAndServeTLS(":4000", *crtPath, *keyPath, nil))
+
+	if *config == "dev" {
+		log.Fatal(http.ListenAndServe(":80", nil))
+	} else if *config == "prod" {
+		certManager := autocert.Manager{
+			Prompt:     autocert.AcceptTOS,
+			HostPolicy: autocert.HostWhitelist("henrywfisher.com", "www.henrywfisher.com"),
+			Cache:      autocert.DirCache("."),
+		}
+
+		server := &http.Server{
+			Addr: ":https",
+			TLSConfig: &tls.Config{
+				GetCertificate: certManager.GetCertificate,
+			},
+		}
+
+		go http.ListenAndServe(":http", handlers.LoggingHandler(os.Stdout, certManager.HTTPHandler(nil)))
+
+		log.Fatal(server.ListenAndServeTLS("", ""))
+	} else {
+		log.Fatal("invalid configuration")
+	}
 }
