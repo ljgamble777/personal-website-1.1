0291d8a8009d66658509fb3e69cb7db1b0b5952d
Henry Fisher <henry.wfisher@gmail.com>
2021-01-12 11:40:03 -0500

    fixes to deploy script and adding email link

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 GOOS=linux go build -o webserver &&
-ssh ec2-18-144-24-104.us-west-1.compute.amazonaws.com 'sudo systemctl stop webserver && mkdir -p personal-website' &&
-rsync -v webserver ec2-18-144-24-104.us-west-1.compute.amazonaws.com:/home/ec2-user/personal-website/ &&
-rsync -rv --exclude=.DS_Store static ec2-18-144-24-104.us-west-1.compute.amazonaws.com:/home/ec2-user/personal-website/ &&
-ssh ec2-18-144-24-104.us-west-1.compute.amazonaws.com 'sudo systemctl start webserver' &&
+ssh ec2-184-169-208-236.us-west-1.compute.amazonaws.com 'sudo systemctl stop webserver && mkdir -p personal-website' &&
+rsync -v webserver ec2-184-169-208-236.us-west-1.compute.amazonaws.com:/home/ec2-user/personal-website/ &&
+rsync -rv --exclude=.DS_Store static ec2-184-169-208-236.us-west-1.compute.amazonaws.com:/home/ec2-user/personal-website/ &&
+ssh ec2-184-169-208-236.us-west-1.compute.amazonaws.com 'sudo systemctl start webserver' &&
 rm webserver
