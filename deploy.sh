d88fcd46f521d6701edb10221c9adb84cf10e6e7
Henry Fisher <henry.wfisher@gmail.com>
2021-11-07 00:57:23 +0000

    dev ops: improved deploy script

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,5 @@
 GOOS=linux go build -o webserver &&
 ssh ec2-184-169-208-236.us-west-1.compute.amazonaws.com 'sudo systemctl stop webserver && mkdir -p personal-website' &&
 rsync -v webserver ec2-184-169-208-236.us-west-1.compute.amazonaws.com:/home/ec2-user/personal-website/ &&
-rsync -rv --exclude=.DS_Store static ec2-184-169-208-236.us-west-1.compute.amazonaws.com:/home/ec2-user/personal-website/ &&
 ssh ec2-184-169-208-236.us-west-1.compute.amazonaws.com 'sudo systemctl start webserver' &&
 rm webserver
