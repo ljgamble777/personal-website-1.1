cefa059233c4529b55380be6fba7f0c78f3d5024
ljgamble777 <102543563+ljgamble777@users.noreply.github.com>
2022-03-29 04:44:03 -0600

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 GOOS=linux go build -o webserver &&
-ssh ec2-184-169-208-236.us-west-1.compute.amazonaws.com 'sudo systemctl stop webserver && mkdir -p personal-website' &&
+ssh ec2-184-169-208-236.us-west-1.compute.amazonaws.com 'sudo systemctl stop webserver && mkdir -p personal-website-1' &&
 rsync -v webserver ec2-184-169-208-236.us-west-1.compute.amazonaws.com:/home/ec2-user/personal-website/ &&
 ssh ec2-184-169-208-236.us-west-1.compute.amazonaws.com 'sudo systemctl start webserver' &&
 rm webserver
